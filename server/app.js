567050adc2dd6cae11f0f63343d03bfd29465b45
José Carneiro <contact@josecarnei.ro>
2021-11-02 20:07:05 +0000

    Set samesite and secure properties on cookie

--- server/app.js
+++ server/app.js
@@ -37,8 +37,8 @@ app.use(
     cookie: {
       maxAge: 15 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: 'none',
-      secure: false
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : false,
+      secure: process.env.NODE_ENV === 'production'
     },
     store: connectMongo.create({
       mongoUrl: process.env.MONGODB_URI,
