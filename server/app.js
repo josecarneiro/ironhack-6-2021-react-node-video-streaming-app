feffc8b93e662cad550d2ef00db247b582e412c2
José Carneiro <contact@josecarnei.ro>
2021-11-02 20:10:12 +0000

    Add proxy property to express session cookie configuration

--- server/app.js
+++ server/app.js
@@ -34,6 +34,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: false,
+    proxy: true,
     cookie: {
       maxAge: 15 * 24 * 60 * 60 * 1000,
       httpOnly: true,
